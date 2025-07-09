d37d15dbe205f746d718d72b5202e9b3fe89e538
IceVFC <marioandrad@gmail.com>
2025-07-09 16:05:15 +0100

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,17 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'José Montanelas' -- Autor do Script 
+description 'Script de Assalto a NPCs'
+version '1.0.0'
+
+shared_scripts {
+    'config.lua' -- Onde estão todas as configurações
+}
+
+client_scripts {
+    'client.lua' -- Lógica do lado do cliente (detalhada no próximo passo)
+}
+
+server_scripts {
+    'server.lua' -- Lógica do lado do servidor (detalhada depois)
